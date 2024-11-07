c330f5f64705a213e887b49cb26ab4b049a1dcfe
Will Scott <willscott.dev@gmail.com>
2024-11-07 11:44:52 +0000

    Added checkbox with 3 states and click handler

--- src/components/checkbox.test.tsx
+++ src/components/checkbox.test.tsx
@@ -1,12 +1,38 @@
-import { render, screen } from "@testing-library/react";
-import { describe, it } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
 
 import Checkbox from "./checkbox";
 
 describe("Checkbox", () => {
   it("has 3 states", () => {
-    render(<Checkbox />);
+    const { rerender } = render(<Checkbox />);
+    const checkbox = screen.getByRole<HTMLInputElement>("checkbox");
+    expect(checkbox.indeterminate).toBeTruthy();
+    expect(checkbox.checked).toBeFalsy();
 
-    screen.debug();
+    rerender(<Checkbox checked />);
+    expect(checkbox.indeterminate).toBeFalsy();
+    expect(checkbox.checked).toBeTruthy();
+
+    rerender(<Checkbox checked={false} />);
+    expect(checkbox.indeterminate).toBeFalsy();
+    expect(checkbox.checked).toBeFalsy();
+  });
+
+  it("handles clicks", () => {
+    const onChange = vi.fn();
+    render(<Checkbox onChange={onChange} />);
+
+    const checkbox = screen.getByRole<HTMLInputElement>("checkbox");
+
+    fireEvent.click(checkbox);
+
+    expect(onChange).toHaveBeenCalled();
+    expect(checkbox.checked).toBeTruthy();
+
+    fireEvent.click(checkbox);
+
+    expect(onChange).toHaveBeenCalled();
+    expect(checkbox.checked).toBeFalsy();
   });
 });
--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -1,3 +1,39 @@
-export default function Checkbox() {
-  return <></>;
+import { ComponentProps, MouseEvent, useEffect, useRef } from "react";
+
+type Props = ComponentProps<"input"> & {
+  checked?: boolean;
+  onChange?: (e: MouseEvent<HTMLInputElement>) => void;
+};
+
+export default function Checkbox(props: Props) {
+  const { checked, onChange } = props;
+
+  const checkboxRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = (e: MouseEvent<HTMLInputElement>) => {
+    onChange?.(e);
+  };
+
+  useEffect(() => {
+    if (checkboxRef.current) {
+      if (checked) {
+        checkboxRef.current.checked = true;
+        checkboxRef.current.indeterminate = false;
+      } else if (checked === false) {
+        checkboxRef.current.checked = false;
+        checkboxRef.current.indeterminate = false;
+      } else {
+        checkboxRef.current.indeterminate = true;
+      }
+    }
+  }, [checked]);
+
+  return (
+    <input
+      ref={checkboxRef}
+      type="checkbox"
+      onClick={handleClick}
+      onChange={onChange}
+    />
+  );
 }
